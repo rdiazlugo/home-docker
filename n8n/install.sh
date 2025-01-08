ff82924f68f7b1ee9181a4e9f0a52d5766a7adb8
Reinaldo Díaz Lugo <hi@rdiazlugo.dev>
2025-01-08 22:46:47 +0100

    Add n8n (#1)
    * add install script
    * improve common functions
    * add readme
    * create default .env on install script

---
+++ n8n/install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+clear
+source <(wget -qO- https://raw.githubusercontent.com/rdiazlugo/home-docker/refs/heads/add-n8n/scripts/common.func)
+
+# App Default Values
+APP="n8n"
+PODMAN_NAME="n8n"
+PODMAN_IMAGE="docker.n8n.io/n8nio/n8n"
+
+# Setup
+variables
+header_info
+
+# Update OS & Install Podman
+podman_install
+
+# Stop, delete current containers and pull latest version
+podman_container_maintenance
+
+# Create .env file from example if not existent
+if [ ! -f .env ]; then
+  wget --no-cache -O .env https://raw.githubusercontent.com/rdiazlugo/home-docker/refs/heads/add-n8n/n8n/.env.example
+fi
+# Check if .env file exists
+check_file_exists ".env"
+
+# Create data volumes
+DATA_VOLUME="${APP}_data"
+podman_create_volume "$DATA_VOLUME"
+
+# Run with Podman
+podman run -d \
+  --name "$PODMAN_NAME" \
+  --hostname "$PODMAN_NAME" \
+  --env-file .env \
+  --network host \
+  --restart always \
+  -v "${DATA_VOLUME}:/home/node/.n8n" \
+  "$PODMAN_IMAGE"
+
+set_reboot_cron
+podman_images_cleanup
