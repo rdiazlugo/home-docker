d45bf8776ab85a7be6f12be401f23477625a39de
Reinaldo Díaz Lugo <hi@rdiazlugo.dev>
2025-01-15 07:28:06 +0100

    Add home assistant (#2)
    * add home assistant install scripts
    
    * update scripts to point to master

---
+++ home_assistant/install.podman.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+clear
+source <(wget -qO- https://raw.githubusercontent.com/rdiazlugo/home-docker/refs/heads/master/scripts/common.func)
+
+# App Default Values
+APP="Home Assistant"
+
+# Setup
+variables
+header_info
+
+# Update OS & Install Podman
+podman_install
+
+# Create .env file from example if not existent
+if [ ! -f .env ]; then
+  wget -O .env https://raw.githubusercontent.com/rdiazlugo/home-docker/refs/heads/master/home_assistant/.env.example
+fi
+
+# Create .env file from example if not existent
+if [ ! -f docker-compose.yml ]; then
+  wget -O docker-compose.yml https://raw.githubusercontent.com/rdiazlugo/home-docker/refs/heads/master/home_assistant/docker-compose.podman.yml
+fi
+
+# Check if .env file exists
+check_file_exists ".env"
+
+mkdir -p ha_config
+mkdir -p esphome_config
+
+# Pull latest version
+podman-compose pull
+# Stop and remove older version
+podman-compose down
+# Start the container
+podman-compose up -d
+
+set_reboot_cron
+podman_images_cleanup
